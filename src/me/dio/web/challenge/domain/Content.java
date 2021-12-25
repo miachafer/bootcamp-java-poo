b9f057117e6bd080e46e4e6cc86064ca3e2e6ae4
miachafer <miachafer@gmail.com>
2021-12-25 20:18:44 -0300

    updating some words on comments

--- src/me/dio/web/challenge/domain/Content.java
+++ src/me/dio/web/challenge/domain/Content.java
@@ -2,14 +2,14 @@ package me.dio.web.challenge.domain;
 
 public abstract class Content {
 
-    // the protected means that this father class will only be accessed by its sons.
+    // the protected means that this parent class will only be accessed by its children.
     // the final means that the variable is a constant
     protected static final double XP_default = 10d;
     private String title;
     private String description;
 
     // the abstract means that the object cannot be instantied.
-    // this class only exists to be used by its sons, so there's no need for instantiation.
+    // this class only exists to be inherited by its children, so there's no need for instantiation.
     public abstract double calculateXp();
 
     public String getTitle() {
